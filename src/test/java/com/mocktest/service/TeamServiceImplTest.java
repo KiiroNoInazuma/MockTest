4229da9cebe99e29e94ac3797c89f72d7411e1ae
John Rey <adioskid@mail.ru>
2023-05-28 01:32:44 +0300

    testON

---
+++ src/test/java/com/mocktest/service/TeamServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.mocktest.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamServiceImplTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void findTeamsAsString() {
+    }
+
+    @Test
+    void findTeams() {
+    }
+
+    @Test
+    void findTeamsByPrefix() {
+    }
+}
